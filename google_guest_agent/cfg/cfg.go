1ac84b1a43de64a448d4a1fbfd3eb0ac0cf7c21c
Alexander Mokin <ya-mokin@yandex-team.ru>
2024-02-01 18:20:42 +0300

    Fixed removeWatcher method in events.go and changed default config

--- google_guest_agent/cfg/cfg.go
+++ google_guest_agent/cfg/cfg.go
@@ -1,3 +1,5 @@
+// Please note that the code below is modified by YANDEX LLC
+
 //  Copyright 2023 Google Inc. All Rights Reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
@@ -89,7 +91,7 @@ ip_forwarding = true
 setup = true
 
 [OSLogin]
-cert_authentication = true
+cert_authentication = false
 
 [MDS]
 mtls_bootstrapping_enabled = true
